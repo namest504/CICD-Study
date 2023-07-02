bd918c3c4c83dc41756fd90800341a7fa2a4db21
List <61047602+namest504@users.noreply.github.com>
2023-07-03 05:07:53 +0900

    feat: 환경 변수

--- src/main/java/com/namest504/deploytest/controller/HelloController.java
+++ src/main/java/com/namest504/deploytest/controller/HelloController.java
@@ -1,18 +1,22 @@
 package com.namest504.deploytest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${TEST_STRING}")
+    private String TEST_STRING;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
 
-    @GetMapping("/hello2")
+    @GetMapping("/String")
     public String hello2() {
-        return "hello2";
+        return TEST_STRING;
     }
 }
