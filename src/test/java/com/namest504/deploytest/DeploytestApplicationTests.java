cb1f9b92889a74f7a790ae85415491934ef4dcfe
List <61047602+namest504@users.noreply.github.com>
2023-07-03 05:12:58 +0900

    delete: test 삭제

--- src/test/java/com/namest504/deploytest/DeploytestApplicationTests.java
+++ src/test/java/com/namest504/deploytest/DeploytestApplicationTests.java
@@ -1,13 +1,11 @@
 package com.namest504.deploytest;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class DeploytestApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
